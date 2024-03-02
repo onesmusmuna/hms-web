0bb07db65de9ee43c0d0d84ef1b94906e2b6ec48
onesmusmuna <onesmusmuna@gmail.com>
2024-03-02 17:37:07 +0300

    login page is loading cookies from the server. JS cannot read it but it is saved in the browser

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,74 @@
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+
 import Nav from "../components/nav";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [plain, setPLain] = useState("");
+
+  // const notify = (text: string) => toast(text);
+
+  const onLogin = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    axios("http://localhost:3000/auth/login", {
+      method: "post",
+      headers: { "Content-type": "application/json" },
+      withCredentials: true,
+      data: { email, plain },
+      // body: JSON.stringify({ email, plain }),
+    }).then((response) => {
+      let a = response.headers["set-cookie"];
+      // document.cookie.push(response.headers["set-cookie"]);
+      console.log(a);
+    });
+
+    // fetch("http://localhost:3000/auth/login", {
+    //   method: "post",
+    //   headers: { "Content-type": "application/json" },
+    //   body: JSON.stringify({ email, plain }),
+    // })
+    //   .then((response) => {
+    //     const cookie = response.headers.getSetCookie();
+    //     console.log(cookie);
+
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     console.log("\n==========\n");
+    //     console.log(data);
+
+    // if (data.status === "bad") {
+    //   return notify(`${data.msg}`);
+    // }
+    // notify(`${data.msg}`);
+    //   });
+  };
+
   return (
     <div>
       <Nav />
       <h1>Login</h1>
+      <ToastContainer position="top-center" autoClose={1500} />
 
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={onLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={plain}
+          onChange={(e) => setPLain(e.target.value)}
+        />
 
-        <button>Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
